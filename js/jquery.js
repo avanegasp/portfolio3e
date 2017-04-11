f9e70a25fc9078dc708ed9b790ab159681a6b1a7
Angie Vanegas <arco527@gmail.com>
2017-04-10 22:45:14 -0500

    Header, nav, banner with effect

---
+++ js/jquery.js
@@ -0,0 +1,5 @@
+
+// estas líneas son para que la página se adapte a cualquier altura en cualquier navegador
+  $(document).ready(function(){
+    $("#particles-js").css({"height":$(window).height() + "px"});
+  });
